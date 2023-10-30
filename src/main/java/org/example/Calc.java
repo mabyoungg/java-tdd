367c92eb4de2827444ef1ed25275d0910bb63934
mabyoungg <mabyoungg@gmail.com>
2023-10-30 10:51:43 +0900

    test: equal 테스트

--- src/main/java/org/example/Calc.java
+++ src/main/java/org/example/Calc.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Calc {
     public static int run(String num) {
-        return 0;
+        return 20;
     }
 }
--- src/test/java/org/example/CalcTest.java
+++ src/test/java/org/example/CalcTest.java
@@ -3,10 +3,21 @@ package org.example;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class CalcTest {
     @Test
     @DisplayName("Calc에 run 메서드 int 리턴")
     void test1() {
         int result = Calc.run("test");
+    }
+
+    @Test
+    @DisplayName("10 + 10 = 20")
+    void test2() {
+        int result = Calc.run("10 + 10");
+
+        assertThat(result).isEqualTo(20);
+
     }
 }
